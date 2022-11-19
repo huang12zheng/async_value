3f79d605b15b8adb0a636e87d57b9f6b020f59b4
J-P Nurmi <jpnurmi@gmail.com>
2022-11-19 07:42:46 +0100

    Export AsyncValue & friends from Riverpod

--- lib/async_value.dart
+++ lib/async_value.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A utility for safely manipulating asynchronous data.
 library async_value;
 
-export 'src/state.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'package:riverpod/riverpod.dart'
+    show AsyncData, AsyncError, AsyncLoading, AsyncValue, AsyncValueX;
